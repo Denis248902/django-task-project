735a6bc74c68e84bb3a5c4471720d1e4d2bd5fad
Денис Постовойтов <denisdotniy@yandex.ru>
2026-07-27 20:42:15 +0300

    fix: correct list_display in admin (full_name instead of first_name/last_name)

--- emp_app/admin.py
+++ emp_app/admin.py
@@ -1,17 +1,32 @@
 from django.contrib import admin
 from .models import Skill, EmployeeProfile, EmployeeSkill, EmployeeImage
 
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
 class EmployeeSkillInline(admin.TabularInline):
     model = EmployeeSkill
     extra = 1
 
-class EmployeeImageInline(admin.StackedInline):
+class EmployeeImageInline(admin.TabularInline):
     model = EmployeeImage
     extra = 1
+    fields = ('image', 'order_index')
 
 @admin.register(EmployeeProfile)
 class EmployeeProfileAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'gender')
+    # ВАЖНО: используем только реально существующие поля модели
+    list_display = ('full_name', 'gender', 'position')
     inlines = [EmployeeSkillInline, EmployeeImageInline]
+    search_fields = ('full_name',)
 
-admin.site.register(Skill)
+@admin.register(EmployeeSkill)
+class EmployeeSkillAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'skill', 'level')
+    list_filter = ('skill',)
+
+@admin.register(EmployeeImage)
+class EmployeeImageAdmin(admin.ModelAdmin):
+    list_display = ('employee', 'order_index', 'image')
+    list_filter = ('employee',)
